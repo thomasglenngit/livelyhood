06fa3818b2d1478ca44f6de6b56d3b572fcca08b
Thomas Glenn <thomasgrantglenn@mac.com>
2021-05-13 00:31:44 -0600

    add geoCodeTranslate function to Neighbor component.

--- src/components/Loc.js
+++ src/components/Loc.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import Neighbor from './Neighbor'
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import { NeighborList } from './NeighborList'
 import CurrentLocation from './LocMap';
 
@@ -19,61 +18,6 @@ export class MapContainer extends Component {
     neighborPlace: {}
   };
 
-  geoCodeTranslate() {
-    const location = `${Neighbor.address}`
-    
-    // document.getElementById('location-input').value;
-
-      axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
-        params:{
-          address:location,
-          key: process.env.REACT_APP_GEOCODE_KEY
-        }
-      })
-      .then(function(response){
-        // Log full response
-        console.log(response);
-
-        // Formatted Address
-        const formattedAddress = response.data.results[0].formatted_address;
-        const formattedAddressOutput = `
-          <ul class="list-group">
-            <li class="list-group-item">${formattedAddress}</li>
-          </ul>
-        `;
-
-        // Address Components
-        const addressComponents = response.data.results[0].address_components;
-        const addressComponentsOutput = '<ul class="list-group">';
-        for(const i = 0;i < addressComponents.length; i++){
-          addressComponentsOutput += `
-            <li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>
-          `;
-        }
-        addressComponentsOutput += '</ul>';
-
-        // Geometry
-        const lat = response.data.results[0].geometry.location.lat;
-        const lng = response.data.results[0].geometry.location.lng;
-        const geometryOutput = `
-          <ul class="list-group">
-            <li class="list-group-item"><strong>Latitude</strong>: ${lat}</li>
-            <li class="list-group-item"><strong>Longitude</strong>: ${lng}</li>
-          </ul>
-        `;
-
-        // Output to app
-        document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
-        document.getElementById('address-components').innerHTML = addressComponentsOutput;
-        document.getElementById('geometry').innerHTML = geometryOutput;
-      })
-      .catch(function(error){
-        console.log(error);
-      });
-    this.setState({
-      neighborPlace: {}
-    })
-  }
 
   onMarkerClick = (props, marker, e) => // clicking on an EXISTING MARKER
     this.setState({
@@ -99,8 +43,10 @@ export class MapContainer extends Component {
           style={mapStyles}
           initialCenter={
             {
-              lat: 45.5051,
-              lng: -122.6750
+              lat: `${Neighbor.lat}`,
+              lng: `${Neighbor.lng}`
+              // lat: 45.5051,
+              // lng: -122.6750
             }
           }
         >
--- src/components/Neighbor.js
+++ src/components/Neighbor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import { locationChange } from './Loc'
+import axios from 'axios'
 import { Link } from 'react-scroll'
 import { Card } from 'react-bootstrap';
 import './Cards/card-style.css';
@@ -8,12 +8,67 @@ import './App.css'
 // import { CardDeck } from 'react-bootstrap';
 
 
+
 const Neighbor = props => {
+
+  const geoCodeTranslate = () => {
+
+    const location = `${Neighbor.address}`
+  
+    // document.getElementById('location-input').value;
+  
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: location,
+        key: process.env.REACT_APP_GEOCODE_KEY
+      }
+    })
+      .then(function (response) {
+        // Log full response
+        console.log(response);
+  
+        // Formatted Address
+        const formattedAddress = response.data.results[0].formatted_address;
+        const formattedAddressOutput = `
+          <ul class="list-group">
+            <li class="list-group-item">${formattedAddress}</li>
+          </ul>
+        `;
+  
+        // Address Components
+        const addressComponents = response.data.results[0].address_components;
+        const addressComponentsOutput = '<ul class="list-group">';
+        for (const i = 0; i < addressComponents.length; i++) {
+          addressComponentsOutput += `
+            <li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>
+          `;
+        }
+        addressComponentsOutput += '</ul>';
+  
+        // Geometry
+        const lat = response.data.results[0].geometry.location.lat;
+        const lng = response.data.results[0].geometry.location.lng;
+        const geometryOutput = `
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Latitude</strong>: ${lat}</li>
+            <li class="list-group-item"><strong>Longitude</strong>: ${lng}</li>
+          </ul>
+        `;
+  
+        // Output to app
+        document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+        document.getElementById('address-components').innerHTML = addressComponentsOutput;
+        document.getElementById('geometry').innerHTML = geometryOutput;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  
+  }
+
   return (
     <React.Fragment>
-      {/* <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
-        <link rel="stylesheet" type="text/css" media="all" href="css/960.css" /> */}
+
       <div className="card text-center shadow">
         <div className="overflow">
           {/* <img src={props.imgsrc} alt='Image 1' className='card-img-top'/> */}
@@ -27,11 +82,10 @@ const Neighbor = props => {
             <p><strong>Tools:</strong>   {props.tools}</p>
           </div>
         </div>
-          <div className="btn btn-outline-success" onClick={() => props.whenNeighborClicked(props.id)}>Edit Details</div>
-          <Link to="map-space"><div className="btn btn-outline-info">
+        <div className="btn btn-outline-success" onClick={() => props.whenNeighborClicked(props.id)}>Edit Details</div>
+        <Link to="map-space"><div className="btn btn-outline-info">
+          {geoCodeTranslate()}
           Locate</div></Link>
-          
-        
       </div>
     </React.Fragment>
   );
